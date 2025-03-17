cf9d3b78c11fa5f280658c870ce5b545f5c631ad
Yiping Kang <yipingkang93@gmail.com>
2025-03-17 18:25:02 -0400

    Add allowed host

--- littleX_FE/vite.config.js
+++ littleX_FE/vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./src", // Point to the 'src' folder
+  server: {
+    port: 3000,  // or your preferred port
+    host: true,  // Allows external access
+    allowedHosts: ['x.jaseci.org'],  // Allow your domain
+  }
 });
